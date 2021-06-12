9bad1ea3a202e43932c8ed10127d378295af52a1
张亚飞 <672233580@qq.com>
2021-06-12 19:31:05 +0800

    Initial Commit

---
+++ ScratchCard/ScratchCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchCardApp.swift
+//  ScratchCard
+//
+//  Created by 张亚飞 on 2021/6/12.
+//
+
+import SwiftUI
+
+@main
+struct ScratchCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
